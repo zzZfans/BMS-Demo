4577b3db14a245059838fc4c7a29b7ad47fa5be0
Zfans <zzzfans@qq.com>
2021-06-15 19:00:28 +0800

    Initial commit

---
+++ src/main/java/com/zfans/bmsdemo/BmsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zfans.bmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zfans/bmsdemo/BmsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zfans.bmsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
